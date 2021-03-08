99e738578d6154f44cb2e2b3d93f688fa0cfd2b9
yunho LEE <mmm01230@gmail.com>
2021-03-08 09:33:41 +0900

    초기 커밋.

---
+++ HelloWord/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWord
+{
+	static class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("학번 이름");
+		}
+	}
+}
